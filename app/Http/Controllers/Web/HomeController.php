ab7d60e40709606ebd50392b248632d538568592
Dzul Maarij <fathesshop@gmail.com>
2021-01-21 15:47:22 +0700

    banksampah

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\User;
-use App\Model\Keuangan;
-use App\Model\Penjualan;
-use App\Model\Penyetoran;
-use App\Model\Jenis;
-use App\Model\Sampah;
+use App\Models\User;
+use App\Models\Finance;
+use App\Models\Sale;
+use App\Models\Deposit;
+use App\Models\Trash;
+use App\Models\Warehouse;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Model\Penarikan;
+use App\Models\Withdrawal;
 
 class HomeController extends Controller
 {
@@ -25,12 +25,12 @@ class HomeController extends Controller
 
         // Card
         $user = User::where('role_id', 1)->where('deleted_at', null)->count();
-        $keuangan = Keuangan::latest()->first('saldo');
-        $penjualan = Penjualan::whereMonth('created_at', $month)->sum('penghasilan');
-        $transaksi = Penyetoran::whereMonth('created_at', $month)->count();;
+        $Finance = Finance::latest()->first('saldo');
+        $Sale = Sale::whereMonth('created_at', $month)->sum('penghasilan');
+        $transaksi = Deposit::whereMonth('created_at', $month)->count();;
 
         // Pie grafik
-        $jenis_sampah = Jenis::all();
+        $jenis_sampah = Trash::all();
         foreach ($jenis_sampah as $value) {
             $jenis[] = $value->jenis_sampah;
         }
@@ -39,7 +39,7 @@ class HomeController extends Controller
             $warna[] = $value->warna;
         }
 
-        $sampahh = Sampah::all();
+        $sampahh = Warehouse::all();
         foreach ($sampahh as $value) {
             $sampah[] = $value->berat;
         }
@@ -47,13 +47,13 @@ class HomeController extends Controller
         // chart penghasilan
         if (request()->url() == 'http://localhost:8000/home') {
             // query mysql untuk localhost
-            $penghasilann = DB::table("penjualans")
+            $penghasilann = DB::table("Sales")
                 ->select(DB::raw("(SUM(penghasilan)) as penghasilan"))
                 ->groupBy(DB::raw("MONTH(created_at)"))
                 ->get();
         } else {
             // Untuk production query pgsql heroku
-            $penghasilann = DB::table("penjualans")
+            $penghasilann = DB::table("Sales")
                 ->select(DB::raw("DATE_TRUNC('month',created_at) AS bulan, SUM(penghasilan) as penghasilan"))
                 ->groupBy(DB::raw("DATE_TRUNC('month',created_at)"))
                 ->get();
@@ -65,12 +65,12 @@ class HomeController extends Controller
             $penghasilan[] =  $value->penghasilan;
         }
 
-        return view('pages.home', compact('user', 'keuangan', 'penjualan', 'transaksi', 'jenis', 'sampah', 'warna', 'jenis_sampah', 'penghasilan'));
+        return view('pages.home', compact('user', 'Finance', 'Sale', 'transaksi', 'jenis', 'sampah', 'warna', 'jenis_sampah', 'penghasilan'));
     }
 
     public function alert()
     {
-        $permintaan = Penarikan::where('status', 1)->count();
+        $permintaan = Withdrawal::where('status', 1)->count();
 
         echo json_encode($permintaan);
     }
