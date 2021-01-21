69b41802cb428c5a47100e3de607991dfc412273
Dzul Maarij <fathesshop@gmail.com>
2021-01-22 00:03:51 +0700

    banksampah

--- app/Http/Controllers/Admin/TrashController.php
+++ app/Http/Controllers/Admin/TrashController.php
@@ -91,7 +91,7 @@ class TrashController extends Controller
         $trash = Trash::find($id);
         $data = ['trash' => $trash];
 
-        return view('admin.trash.show');
+        return view('admin.trash.show')->with($data);
     }
 
     /**
@@ -105,7 +105,7 @@ class TrashController extends Controller
         $trash = Trash::find($id);
         $data = ['trash' => $trash];
 
-        return view('admin.trash.edit')->with('data');
+        return view('admin.trash.edit')->with($data);
     }
 
     /**
