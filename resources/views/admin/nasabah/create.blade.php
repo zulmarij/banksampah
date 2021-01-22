9024a533074948d7d7cb403dc4e4d03ba4e30893
Dzul Maarij <fathesshop@gmail.com>
2021-01-22 21:29:35 +0700

    banksampah

--- resources/views/admin/nasabah/create.blade.php
+++ resources/views/admin/nasabah/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="row">
     <div class="col-12">
-        {{ Form::open('route'=>'nasabah.store') }}
+        {{ Form::open(['route'=>'nasabah.store']) }}
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">Added Nasabah</h3>
