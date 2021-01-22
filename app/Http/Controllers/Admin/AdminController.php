d552468a92e513578e0ebf16f4c743f7ec619eef
Dzul Maarij <fathesshop@gmail.com>
2021-01-22 15:07:04 +0700

    banksampah

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Finance;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
-    {
-        return view('admin.admin');
+    {   
+        $finance = Finance::latest()->first('balance');
+        $data = ['finance' => $finance];
+        return view('admin.admin')->with($data);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,8 +8,7 @@
               <div class="info-box-content">
                 <span class="info-box-text">CPU Traffic</span>
                 <span class="info-box-number">
-                  10
-                  <small>%</small>
+                  {{ $finance }}
                 </span>
               </div>
               <!-- /.info-box-content -->
