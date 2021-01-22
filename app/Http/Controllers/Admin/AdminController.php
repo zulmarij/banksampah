e238d755a9af46a1903991953cbd11e4188a1a76
Dzul Maarij <fathesshop@gmail.com>
2021-01-22 16:27:14 +0700

    banksampah

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Deposit;
 use App\Models\Finance;
 use App\Models\Sale;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {   
-        $month = date('m');
-
-        $finance = Finance::latest()->first();
+        $user = User::count();
+        $finance = Finance::latest()->first('balance');
         $sale = Sale::sum('revenue');
+        $deposit = Deposit::count();
 
         // $data = ['finance' => $finance];
-        return view('admin.admin', compact('finance', 'sale'));
+        return view('admin.admin', compact('finance', 'sale', 'user', 'deposit'));
     }
 }
