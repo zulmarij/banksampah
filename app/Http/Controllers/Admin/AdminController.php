0ca17eea884247f1bcf9b7d7ee7b3028b36e965c
Dzul Maarij <fathesshop@gmail.com>
2021-01-22 16:02:49 +0700

    banksampah

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Finance;
+use App\Models\Sale;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {   
+        $month = date('m');
+
         $finance = Finance::latest()->first();
-        $data = ['finance' => $finance];
-        return view('admin.admin')->with($data);
+        $sale = Sale::whereMonth('created_at', $month)->sum('revenue');
+
+        // $data = ['finance' => $finance];
+        return view('admin.admin', compact('finance', 'sale'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
               <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
               <div class="info-box-content">
-                <span class="info-box-text">CPU Traffic</span>
+                <span class="info-box-text">Balance</span>
                 <span class="info-box-number">
                  Rp. {{ $finance['balance'] }}
                 </span>
@@ -22,7 +22,7 @@
 
               <div class="info-box-content">
                 <span class="info-box-text">Likes</span>
-                <span class="info-box-number">41,410</span>
+                <span class="info-box-number">{{ $sale[] }}</span>
               </div>
               <!-- /.info-box-content -->
             </div>
