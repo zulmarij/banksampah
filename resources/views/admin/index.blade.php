a15b4632247fc51511afc7d8728a13fa9adf55f1
Dzul Maarij <fathesshop@gmail.com>
2021-01-22 16:03:53 +0700

    banksampah

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -22,7 +22,7 @@
 
               <div class="info-box-content">
                 <span class="info-box-text">Likes</span>
-                <span class="info-box-number">{{ $sale[] }}</span>
+                <span class="info-box-number">{{ $sale }}</span>
               </div>
               <!-- /.info-box-content -->
             </div>
