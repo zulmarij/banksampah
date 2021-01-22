4b8cd50250168fc2a300bca87cd7b16f8f95d437
Dzul Maarij <fathesshop@gmail.com>
2021-01-23 06:18:01 +0700

    banksampah

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,8 +1,8 @@
  <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+     <i class="fa fa-love"></i> Thanks to Pondok Programmer
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2014-2021 <a href="{{ URL::to('/admin') }}">Sampah Bank</a>.</strong> All rights reserved.
   </footer>
