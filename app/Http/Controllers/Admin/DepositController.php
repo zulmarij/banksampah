688d3554393c36cce574b89d3b237f6b02f6b163
Dzul Maarij <fathesshop@gmail.com>
2021-01-24 20:07:51 +0700

    banksampah

--- app/Http/Controllers/Admin/DepositController.php
+++ app/Http/Controllers/Admin/DepositController.php
@@ -11,12 +11,12 @@ class DepositController extends Controller
 {
     public function index()
     {
-        $revenue = Deposit::sum('revenue')->get();
+        $revenue = Deposit::sum('revenue');
         $weight = Deposit::sum('weight');
         $trash = Trash::count('trash');
         $report = Deposit::count();
         $deposit = Deposit::with('user', 'trash')->get();
 
-        return view('admin.deposit.index', compact('deposit'));
+        return view('admin.deposit.index', compact('deposit, report, trash, weight, revenue'));
     }
 }
