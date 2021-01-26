280d98b4ac22c3c5d056898858cd33207b25d479
Dzul Maarij <fathesshop@gmail.com>
2021-01-26 13:50:25 +0700

    banksampah

--- resources/views/admin/withdrawal/request.blade.php
+++ resources/views/admin/withdrawal/request.blade.php
@@ -29,6 +29,74 @@
                 @foreach ($request as $data)
                 <tr>
                     <td>{{ $data->id }}</td>
+<td>{{ $data->user->name }}</td>
+<td>{{ $data->user->email }}</td>
+<td>{{ $data->account }}</td>
+<td>{{ $data->created_at }}</td>
+<td class="project-actions text-right">
+    <a class="btn btn-primary btn-sm" href="#">
+        <i class="fas fa-folder">
+        </i>
+        View
+    </a>
+    <a class="btn btn-info btn-sm" href="#">
+        <i class="fas fa-pencil-alt">
+        </i>
+        Edit
+    </a>
+    <a class="btn btn-danger btn-sm" href="#">
+        <i class="fas fa-trash">
+        </i>
+        Delete
+    </a>
+</td>
+</tr>
+@endforeach
+</tbody>
+</table>
+</div>
+<!-- /.card-body -->
+</div> --}}
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Projects</h3>
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+    </div>
+    <div class="card-body p-0">
+        <table class="table table-striped projects">
+            <thead>
+                <tr>
+                    <th style="width: 1%">
+                        #
+                    </th>
+                    <th style="width: 20%">
+                        Project Name
+                    </th>
+                    <th style="width: 30%">
+                        Team Members
+                    </th>
+                    <th>
+                        Project Progress
+                    </th>
+                    <th style="width: 8%" class="text-center">
+                        Status
+                    </th>
+                    <th style="width: 20%">
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($request as $data)
+                <tr>
+                    <td>{{ $data->id }}</td>
                     <td>{{ $data->user->name }}</td>
                     <td>{{ $data->user->email }}</td>
                     <td>{{ $data->account }}</td>
@@ -56,159 +124,5 @@
         </table>
     </div>
     <!-- /.card-body -->
-</div> --}}
-<div class="card">
-    <div class="card-header">
-      <h3 class="card-title">Projects</h3>
-
-      <div class="card-tools">
-        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-          <i class="fas fa-minus"></i>
-        </button>
-        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-          <i class="fas fa-times"></i>
-        </button>
-      </div>
-    </div>
-    <div class="card-body p-0">
-      <table class="table table-striped projects">
-          <thead>
-              <tr>
-                  <th style="width: 1%">
-                      #
-                  </th>
-                  <th style="width: 20%">
-                      Project Name
-                  </th>
-                  <th style="width: 30%">
-                      Team Members
-                  </th>
-                  <th>
-                      Project Progress
-                  </th>
-                  <th style="width: 8%" class="text-center">
-                      Status
-                  </th>
-                  <th style="width: 20%">
-                  </th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr>
-                  <td>
-                      #
-                  </td>
-                  <td>
-                      <a>
-                          AdminLTE v3
-                      </a>
-                      <br/>
-                      <small>
-                          Created 01.01.2019
-                      </small>
-                  </td>
-                  <td>
-                      <ul class="list-inline">
-                          <li class="list-inline-item">
-                              <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
-                          </li>
-                          <li class="list-inline-item">
-                              <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
-                          </li>
-                          <li class="list-inline-item">
-                              <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
-                          </li>
-                          <li class="list-inline-item">
-                              <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar4.png">
-                          </li>
-                      </ul>
-                  </td>
-                  <td class="project_progress">
-                      <div class="progress progress-sm">
-                          <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 57%">
-                          </div>
-                      </div>
-                      <small>
-                          57% Complete
-                      </small>
-                  </td>
-                  <td class="project-state">
-                      <span class="badge badge-success">Success</span>
-                  </td>
-                  <td class="project-actions text-right">
-                      <a class="btn btn-primary btn-sm" href="#">
-                          <i class="fas fa-folder">
-                          </i>
-                          View
-                      </a>
-                      <a class="btn btn-info btn-sm" href="#">
-                          <i class="fas fa-pencil-alt">
-                          </i>
-                          Edit
-                      </a>
-                      <a class="btn btn-danger btn-sm" href="#">
-                          <i class="fas fa-trash">
-                          </i>
-                          Delete
-                      </a>
-                  </td>
-              </tr>
-              <tr>
-                  <td>
-                      #
-                  </td>
-                  <td>
-                      <a>
-                          AdminLTE v3
-                      </a>
-                      <br/>
-                      <small>
-                          Created 01.01.2019
-                      </small>
-                  </td>
-                  <td>
-                      <ul class="list-inline">
-                          <li class="list-inline-item">
-                              <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
-                          </li>
-                          <li class="list-inline-item">
-                              <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
-                          </li>
-                      </ul>
-                  </td>
-                  <td class="project_progress">
-                      <div class="progress progress-sm">
-                          <div class="progress-bar bg-green" role="progressbar" aria-valuenow="47" aria-valuemin="0" aria-valuemax="100" style="width: 47%">
-                          </div>
-                      </div>
-                      <small>
-                          47% Complete
-                      </small>
-                  </td>
-                  <td class="project-state">
-                      <span class="badge badge-success">Success</span>
-                  </td>
-                  <td class="project-actions text-right">
-                      <a class="btn btn-primary btn-sm" href="#">
-                          <i class="fas fa-folder">
-                          </i>
-                          View
-                      </a>
-                      <a class="btn btn-info btn-sm" href="#">
-                          <i class="fas fa-pencil-alt">
-                          </i>
-                          Edit
-                      </a>
-                      <a class="btn btn-danger btn-sm" href="#">
-                          <i class="fas fa-trash">
-                          </i>
-                          Delete
-                      </a>
-                  </td>
-              </tr>
-          </tbody>
-      </table>
-    </div>
-    <!-- /.card-body -->
-  </div>
+</div>
 @endsection
