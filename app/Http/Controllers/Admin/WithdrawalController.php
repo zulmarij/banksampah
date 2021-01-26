3296169bcfa8e2e498e6708e0bd4cc5b4b6e420a
Dzul Maarij <fathesshop@gmail.com>
2021-01-26 14:09:11 +0700

    banksampah

--- app/Http/Controllers/Admin/WithdrawalController.php
+++ app/Http/Controllers/Admin/WithdrawalController.php
@@ -15,7 +15,7 @@ class WithdrawalController extends Controller
 
     public function getRequest()
     {
-        $request = Withdrawal::where('status', 1)->get();
+        $request = Withdrawal::where('status', [NULL ,'1'])->get();
 
         return view('admin.withdrawal.request', compact('request'));
     }
