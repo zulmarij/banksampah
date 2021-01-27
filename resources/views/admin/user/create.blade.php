93c59017bde4adc1b935e3c1b73aeda1bd892509
Dzul Maarij <fathesshop@gmail.com>
2021-01-27 09:04:10 +0700

    banksampah

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -14,11 +14,6 @@
                 </div> --}}
             </div>
             <div class="card-body">
-                @if(!empty($errors->all()))
-                <div class="alert alert-danger">
-                    {{ Html::ul($errors->all())}}
-                </div>
-                @endif
                 <div class="form-group">
                     {{ Form::label('name', 'Name') }}
                     {{ Form::text('name', ['class'=>'form-control', 'placeholder'=>'Name']) }}
@@ -47,64 +42,65 @@
             </div>
         </div>
         <!-- /.card -->
+        {{ Form::close() }}
     </div>
 </div>
 
 {{-- <div class="row">
     <div class="col-12">
         {{ Form::open(['action'=>'Admin\NasabahController@store']) }}
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Added Nasabah</h3>
-            </div>
-            <div class="card-body">
-                @if(!empty($errors->all()))
-                <div class="alert alert-danger">
-                    {{ Html::ul($errors->all())}}
-                </div>
-                @endif
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            {{ Form::label('name', 'Name') }}
-                            {{ Form::text('name', '', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Name']) }}
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            {{ Form::label('email', 'Email') }}
-                            {{ Form::email('email', '', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Email']) }}
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            {{ Form::label('password', 'Password') }}
-                            {{ Form::password('password', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Password']) }}
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            {{ Form::label('password_confirmation', 'Password') }}
-                            {{ Form::password('Password_confrimation', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Password Again']) }}
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            {{ Form::label('role', 'Role') }}
-                            {{ Form::text('role', '', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Role']) }}
-                        </div>
-                    </div>
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Added Nasabah</h3>
+    </div>
+    <div class="card-body">
+        @if(!empty($errors->all()))
+        <div class="alert alert-danger">
+            {{ Html::ul($errors->all())}}
+        </div>
+        @endif
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    {{ Form::label('name', 'Name') }}
+                    {{ Form::text('name', '', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Name']) }}
                 </div>
             </div>
-            <div class="card-footer">
-                <a href="{{ URL::to('admin/nasabah') }}" class="btn btn-outline-info">Back</a>
-                {{ Form::submit('Input', ['class' => 'btn btn-primary pull-right']) }}
+            <div class="col-md-6">
+                <div class="form-group">
+                    {{ Form::label('email', 'Email') }}
+                    {{ Form::email('email', '', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Email']) }}
+                </div>
             </div>
         </div>
-        <!-- </form> -->
-        {{ Form::close() }}
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    {{ Form::label('password', 'Password') }}
+                    {{ Form::password('password', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Password']) }}
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    {{ Form::label('password_confirmation', 'Password') }}
+                    {{ Form::password('Password_confrimation', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Password Again']) }}
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    {{ Form::label('role', 'Role') }}
+                    {{ Form::text('role', '', ['class'=>'form-control', 'placeholder'=>'Enter the Nasabah Role']) }}
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="card-footer">
+        <a href="{{ URL::to('admin/nasabah') }}" class="btn btn-outline-info">Back</a>
+        {{ Form::submit('Input', ['class' => 'btn btn-primary pull-right']) }}
+    </div>
+</div>
+<!-- </form> -->
+{{ Form::close() }}
+</div>
 </div> --}}
 @endsection
