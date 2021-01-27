c956bcd8ea576607ba3108501e9e8492280cd6b6
Dzul Maarij <fathesshop@gmail.com>
2021-01-28 03:59:51 +0700

    banksampah

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 // Route::view('password/reset', 'auth.reset_password')->name('password.reset');
