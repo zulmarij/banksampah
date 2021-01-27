c8fcc2f787d6f124dc7f6d3ad2807b0d8458967f
Dzul Maarij <fathesshop@gmail.com>
2021-01-27 21:35:20 +0700

    banksampah

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,149 +1,68 @@
-{{-- @extends('admin.admin')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email"
-                                class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Lockscreen</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Lockscreen</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('template/plugins/fontawesome-free/css/all.min.css')}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('template/dist/css/adminlte.min.css')}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('template/plugins/fontawesome-free/css/all.min.css')}}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{asset('template/dist/css/adminlte.min.css')}}">
 </head>
-
 <body class="hold-transition lockscreen">
-    <!-- Automatic element centering -->
-    {{ Form::open(['route'=>'{{ route('login') }}']) }}
-    <div class="lockscreen-wrapper">
-        <div class="lockscreen-logo">
-            <a href="{{asset('')}}template/index2.html"><b>Admin</b>LTE</a>
-        </div>
-        <!-- User name -->
-        <div class="lockscreen-name">John Doe</div>
+<!-- Automatic element centering -->
+<div class="lockscreen-wrapper">
+  <div class="lockscreen-logo">
+    <a href="../../index2.html"><b>Admin</b>LTE</a>
+  </div>
+  <!-- User name -->
+  <div class="lockscreen-name">John Doe</div>
 
-        <!-- START LOCK SCREEN ITEM -->
-        <div class="lockscreen-item">
-            <!-- lockscreen image -->
-            <div class="lockscreen-image">
-                <img src="{{asset('template/dist/img/user1-128x128.jpg')}}" alt="User Image">
-            </div>
-            <!-- /.lockscreen-image -->
-            {{ Form::hidden('email', 'admin@gmail.com')}}
-            <!-- lockscreen credentials (contains the form) -->
-            <form class="lockscreen-credentials">
-                <div class="input-group">
-                    {{ Form::password('password', ['class'=>'form-control', 'placeholder'=>'Password']) }}
-                    <div class="input-group-append">
-                        {{ Form::button('<i class="fas fa-arrow-right text-muted"></i>', ['type' => 'submit', 'class' => 'btn'] )  }}
-                    </div>
-                </div>
-            </form>
-            <!-- /.lockscreen credentials -->
-
-        </div>
-        <!-- /.lockscreen-item -->
-        <div class="help-block text-center">
-            Enter your password to retrieve your session
-        </div>
-        <div class="text-center">
-            <a href="login.html">Or sign in as a different user</a>
-        </div>
-        <div class="lockscreen-footer text-center">
-            Copyright &copy; 2014-2020 <b><a href="https://adminlte.io" class="text-black">AdminLTE.io</a></b><br>
-            All rights reserved
-        </div>
+  <!-- START LOCK SCREEN ITEM -->
+  <div class="lockscreen-item">
+    <!-- lockscreen image -->
+    <div class="lockscreen-image">
+      <img src="https://i.ibb.co/cFZfrYC/administrator.png" alt="login" width="128" height="128">
     </div>
-    {{ Form::close() }}
-    <!-- /.center -->
+    <!-- /.lockscreen-image -->
+
+    <!-- lockscreen credentials (contains the form) -->
+    <form class="lockscreen-credentials">
+      <div class="input-group">
+        <input type="password" class="form-control" placeholder="password">
+
+        <div class="input-group-append">
+          <button type="button" class="btn">
+            <i class="fas fa-arrow-right text-muted"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+    <!-- /.lockscreen credentials -->
+
+  </div>
+  <!-- /.lockscreen-item -->
+  <div class="help-block text-center">
+    Enter your password to retrieve your session
+  </div>
+  <div class="text-center">
+    <a href="login.html">Or sign in as a different user</a>
+  </div>
+  <div class="lockscreen-footer text-center">
+    Copyright &copy; 2014-2020 <b><a href="https://adminlte.io" class="text-black">AdminLTE.io</a></b><br>
+    All rights reserved
+  </div>
+</div>
+<!-- /.center -->
 
     <!-- jQuery -->
     <script src="{{asset('template/plugins/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap 4 -->
     <script src="{{asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 </body>
-
 </html>
+
