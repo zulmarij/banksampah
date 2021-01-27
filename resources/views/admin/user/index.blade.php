c0a63839302adc768fa7ee50f771147ee464a4e9
Dzul Maarij <fathesshop@gmail.com>
2021-01-27 10:25:07 +0700

    banksampah

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,4 +1,4 @@
-@extends('admin/admin')
+@extends('admin/admin',['title' => "User | Sampah Bank"])
 @section('content')
 {{-- <div class="row">
     <div class="col-12">
@@ -7,62 +7,60 @@
                 <h3 class="card-title">Nasabah</h3>
                 <div class="card-tools">
                     <a href="{{ URL::to('/admin/user/create')}}" class="btn btn-dark">
-                        <i class="fa fa-plus"></i>
-                        &nbsp; Add
-                    </a>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr class="text-center">
-                                    <th>ID</th>
-                                    <th>name</th>
-                                    <th>phone</th>
-                                    <th>email</th>
-                                    <th>address</th>
-                                    <th>photo</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($users as $user)
-                                <tr>
-                                    <td class="text-center">{{ $user['id'] }}</td>
-                                    <td>{{ $user['name'] }}</td>
-                                    <td>{{ $user['phone'] }}</td>
-                                    <td>{{ $user['email'] }}</td>
-                                    <td>{{ $user['address'] }}</td>
-                                    <td class="text-center"><img src="{{ $user['photo'] }}" width="100" /></td>
-                                    <td class="text-center">
-                                        <form method="POST" action="{{ URL::to('/admin/user/'.$user['id']) }}">
-                                            {{ csrf_field() }}
-                                            <input type="hidden" name="_method" value="DELETE" />
-                                            <div class="btn-group">
-                                                <a class="btn btn-info"
-                                                    href="{{ URL::to('/admin/user/'.$user['id']) }}"><i
-                                                        class="fa fa-eye"></i></a>
-                                                <a class="btn btn-success"
-                                                    href="{{ URL::to('/admin/user/'.$user['id'].'/edit') }}"><i
-                                                        class="fa fa-edit"></i></a>
-                                                <button type="submit" class="btn btn-danger"><i
-                                                        class="fa fa-eraser"></i></button>
-                                            </div>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+<i class="fa fa-plus"></i>
+&nbsp; Add
+</a>
+</div>
+</div>
+<div class="card-body">
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr class="text-center">
+                        <th>ID</th>
+                        <th>name</th>
+                        <th>phone</th>
+                        <th>email</th>
+                        <th>address</th>
+                        <th>photo</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <td class="text-center">{{ $user['id'] }}</td>
+                        <td>{{ $user['name'] }}</td>
+                        <td>{{ $user['phone'] }}</td>
+                        <td>{{ $user['email'] }}</td>
+                        <td>{{ $user['address'] }}</td>
+                        <td class="text-center"><img src="{{ $user['photo'] }}" width="100" /></td>
+                        <td class="text-center">
+                            <form method="POST" action="{{ URL::to('/admin/user/'.$user['id']) }}">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="_method" value="DELETE" />
+                                <div class="btn-group">
+                                    <a class="btn btn-info" href="{{ URL::to('/admin/user/'.$user['id']) }}"><i
+                                            class="fa fa-eye"></i></a>
+                                    <a class="btn btn-success"
+                                        href="{{ URL::to('/admin/user/'.$user['id'].'/edit') }}"><i
+                                            class="fa fa-edit"></i></a>
+                                    <button type="submit" class="btn btn-danger"><i class="fa fa-eraser"></i></button>
+                                </div>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
 
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
+</div>
+</div>
+</div>
 </div> --}}
 <div class="row">
     <div class="col-12">
@@ -80,32 +78,43 @@
                             <th>phone</th>
                             <th>email</th>
                             <th>address</th>
-                            <th>photo</th>
+                            <th>role</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach($user as $data)
                         <tr>
-                            <td class="text-center">{{ $data['id'] }}</td>
+                            <td>{{ $data['id'] }}</td>
                             <td>{{ $data['name'] }}</td>
                             <td>{{ $data['phone'] }}</td>
                             <td>{{ $data['email'] }}</td>
                             <td>{{ $data['address'] }}</td>
-                            <td class="text-center"><img src="{{ $data['photo'] }}" width="100" /></td>
-                            <td class="text-center">
+                            <td>
+                                @foreach ($user->getRoleNames as $role)
+                                <span class="badge badge-success">{{ $role }}</span>
+                                @endforeach
+                            </td>
+                            <td>
                                 <form method="POST" action="{{ URL::to('/admin/user/'.$data['id']) }}">
                                     {{ csrf_field() }}
                                     <input type="hidden" name="_method" value="DELETE" />
-                                    <div class="btn-group">
-                                        <a class="btn btn-info" href="{{ URL::to('/admin/user/'.$data['id']) }}"><i
-                                                class="fa fa-eye"></i></a>
-                                        <a class="btn btn-success"
-                                            href="{{ URL::to('/admin/user/'.$data['id'].'/edit') }}"><i
-                                                class="fa fa-edit"></i></a>
-                                        <button type="submit" class="btn btn-danger"><i
-                                                class="fa fa-eraser"></i></button>
-                                    </div>
+                                    <a class="btn btn-primary btn-sm" href="{{ URL::to('/admin/user/'.$data['id']) }}">
+                                        <i class="fas fa-eye">
+                                        </i>
+                                        View
+                                    </a>
+                                    <a class="btn btn-info btn-sm"
+                                        href="{{ URL::to('/admin/user/'.$data['id'].'/edit') }}">
+                                        <i class="fas fa-pencil-alt">
+                                        </i>
+                                        Edit
+                                    </a>
+                                    <button type="submit" class="btn btn-danger btn-sm">
+                                        <i class="fas fa-trash">
+                                        </i>
+                                        Delete
+                                    </button>
                                 </form>
                             </td>
                         </tr>
