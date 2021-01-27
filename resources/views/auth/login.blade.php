a743669643b1651e41686182e4c993601855cded
Dzul Maarij <fathesshop@gmail.com>
2021-01-27 21:39:24 +0700

    banksampah

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Lockscreen</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Lockscreen</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet"
-  href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-<!-- Font Awesome -->
-<link rel="stylesheet" href="{{asset('template/plugins/fontawesome-free/css/all.min.css')}}">
-<!-- Theme style -->
-<link rel="stylesheet" href="{{asset('template/dist/css/adminlte.min.css')}}">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('template/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('template/dist/css/adminlte.min.css')}}">
 </head>
+
 <body class="hold-transition lockscreen">
-<!-- Automatic element centering -->
-<div class="lockscreen-wrapper">
-  <div class="lockscreen-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
-  </div>
-  <!-- User name -->
-  <div class="lockscreen-name">John Doe</div>
-
-  <!-- START LOCK SCREEN ITEM -->
-  <div class="lockscreen-item">
-    <!-- lockscreen image -->
-    <div class="lockscreen-image">
-      <img src="https://i.ibb.co/cFZfrYC/administrator.png" alt="login" width="128" height="128">
-    </div>
-    <!-- /.lockscreen-image -->
-
-    <!-- lockscreen credentials (contains the form) -->
-    <form class="lockscreen-credentials">
-      <div class="input-group">
-        <input type="password" class="form-control" placeholder="password">
-
-        <div class="input-group-append">
-          <button type="button" class="btn">
-            <i class="fas fa-arrow-right text-muted"></i>
-          </button>
+    <!-- Automatic element centering -->
+    <div class="lockscreen-wrapper">
+        <div class="lockscreen-logo">
+            <a href="../../index2.html"><b>Admin</b>LTE</a>
         </div>
-      </div>
-    </form>
-    <!-- /.lockscreen credentials -->
+        <!-- User name -->
+        <div class="lockscreen-name">John Doe</div>
 
-  </div>
-  <!-- /.lockscreen-item -->
-  <div class="help-block text-center">
-    Enter your password to retrieve your session
-  </div>
-  <div class="text-center">
-    <a href="login.html">Or sign in as a different user</a>
-  </div>
-  <div class="lockscreen-footer text-center">
-    Copyright &copy; 2014-2020 <b><a href="https://adminlte.io" class="text-black">AdminLTE.io</a></b><br>
-    All rights reserved
-  </div>
-</div>
-<!-- /.center -->
+        <!-- START LOCK SCREEN ITEM -->
+        <div class="lockscreen-item">
+            <!-- lockscreen image -->
+            <div class="lockscreen-image">
+                <img src="https://i.ibb.co/cFZfrYC/administrator.png" alt="login" width="128" height="128">
+            </div>
+            <!-- /.lockscreen-image -->
+
+            <!-- lockscreen credentials (contains the form) -->
+            <form class="lockscreen-credentials" method="POST" action="{{route('login')}}">
+                <div class="input-group">
+                    <input type="hidden" id="email" name="email" value="admin@gmail.com">
+                    <input type="password" class="form-control" placeholder="password">
+
+                    <div class="input-group-append">
+                        <button type="button" class="btn">
+                            <i class="fas fa-arrow-right text-muted"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <!-- /.lockscreen credentials -->
+
+        </div>
+        <!-- /.lockscreen-item -->
+        <div class="help-block text-center">
+            Enter your password to retrieve your session
+        </div>
+        <div class="text-center">
+            <a href="login.html">Or sign in as a different user</a>
+        </div>
+        <div class="lockscreen-footer text-center">
+            Copyright &copy; 2014-2020 <b><a href="https://adminlte.io" class="text-black">AdminLTE.io</a></b><br>
+            All rights reserved
+        </div>
+    </div>
+    <!-- /.center -->
 
     <!-- jQuery -->
     <script src="{{asset('template/plugins/jquery/jquery.min.js')}}"></script>
     <!-- Bootstrap 4 -->
     <script src="{{asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 </body>
-</html>
 
+</html>
