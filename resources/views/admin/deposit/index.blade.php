4f4a9de59045f01c3afc7058e21192dbf1f1688d
Dzul Maarij <fathesshop@gmail.com>
2021-01-28 09:43:27 +0700

    banksampah

--- resources/views/admin/deposit/index.blade.php
+++ resources/views/admin/deposit/index.blade.php
@@ -2,34 +2,29 @@
 @section('content')
 <div class="row">
     <div class="col-md-2">
-        <!-- Widget: user widget style 2 -->
-        <div class="card card-widget widget-user-2">
-            <!-- Add the bg color to the header using any of the bg-* classes -->
-            <div class="widget-user-header bg-warning">
-                <div class="widget-user-image">
-                    <img class="img-circle elevation-2" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
+        <div class="card card-primary collapsed-card">
+            <div class="card-header">
+                <h3 class="card-title">Kertas</h3>
+
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+                    </button>
                 </div>
-                <!-- /.widget-user-image -->
-                <span class="widget-user-username">Kertas</span>
+                <!-- /.card-tools -->
             </div>
-            <div class="card-footer p-0">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        Weight <span class="float-right badge bg-primary">{{ $kertasWeight }}</span>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        Weight <span class="float-right badge bg-primary">{{ $kertasWeight  }}</span>
                     </li>
-                    <li class="nav-item">
-                        Revenue <span class="float-right badge bg-info">{{ $kertasRevenue }}</span>
-                    </li>
-                    <li class="nav-item">
-                        User <span class="float-right badge bg-success">{{ $kertasUser }}</span>
-                    </li>
-                    <li class="nav-item">
-                        Total <span class="float-right badge bg-danger">{{ $kertasTotal }}</span>
-                    </li>
-                </ul>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                  </ul>
             </div>
+            <!-- /.card-body -->
         </div>
-        <!-- /.widget-user -->
+        <!-- /.card -->
     </div>
 </div>
 <div class="row">
@@ -145,17 +140,6 @@
                 </tr>
                 @endforeach
             </tbody>
-            <tfoot>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Trash</th>
-                    <th>Weight</th>
-                    <th>Revenue</th>
-                    <th>Created_at</th>
-                </tr>
-            </tfoot>
         </table>
     </div>
     <!-- /.card-body -->
