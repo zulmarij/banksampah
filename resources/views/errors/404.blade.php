26f99d2d08d673d993b27a1fa964a172cd4ddfe6
Dzul Maarij <fathesshop@gmail.com>
2021-01-30 03:20:55 +0700

    sampahbank

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,3 +3,10 @@
 @section('title', __('Not Found'))
 @section('code', '404')
 @section('message', __('Not Found'))
+<div class="error-content">
+    <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
+    <p>
+      We could not find the page you were looking for.
+      Meanwhile, you may <a href="{{asset('/admin')}}">return to index</a>.
+    </p>
+  </div>
