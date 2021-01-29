238a90b534fe7ba74645d961a4e860bfc1708ebd
Dzul Maarij <fathesshop@gmail.com>
2021-01-29 13:33:31 +0700

    sampahbank

--- app/Http/Controllers/Admin/WarehouseController.php
+++ app/Http/Controllers/Admin/WarehouseController.php
@@ -10,31 +10,14 @@ class WarehouseController extends Controller
 {
     public function index()
     {
-
-        $kertasTotal = Warehouse::where('trash_id', 1)->count();
-        $kertasWeight = Warehouse::where('trash_id', 1)->sum('weight');
-
-        $plastikTotal = Warehouse::where('trash_id', 2)->count();
-        $plastikWeight = Warehouse::where('trash_id', 2)->sum('weight');
-
-        $kacaTotal = Warehouse::where('trash_id', 3)->count();
-        $kacaWeight = Warehouse::where('trash_id', 3)->sum('weight');
-
-        $minyakTotal = Warehouse::where('trash_id', 4)->count();
-        $minyakWeight = Warehouse::where('trash_id', 4)->sum('weight');
-
-        $logamTotal = Warehouse::where('trash_id', 5)->count();
-        $logamWeight = Warehouse::where('trash_id', 5)->sum('weight');
-
-        $elektronikTotal = Warehouse::where('trash_id', 6)->count();
-        $elektronikWeight = Warehouse::where('trash_id', 6)->sum('weight');
-
         $weight = Warehouse::sum('weight');
         $trash = Warehouse::distinct('trash_id')->count();
+        $price =
         $total = Warehouse::count();
 
         $warehouse = Warehouse::with('trash')->get();
 
-        return view('admin.warehouse.index', compact('warehouse', 'total', 'trash', 'weight', 'kertasWeight', 'kertasTotal', 'plastikWeight', 'plastikTotal', 'kacaWeight', 'kacaTotal', 'minyakWeight', 'minyakTotal', 'logamWeight', 'logamTotal', 'elektronikWeight', 'elektronikTotal'));
+        $dd = dd($warehouse);
+        return view('admin.warehouse.index', compact('warehouse', 'total', 'trash', 'weight', 'dd'));
     }
 }
