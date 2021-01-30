cf22426e2d3ac6a1ea209556b9818e36a8e5dcd8
Dzul Maarij <fathesshop@gmail.com>
2021-01-30 10:03:58 +0700

    sampahbank

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -128,7 +128,7 @@
         <li class="nav-item">
             <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                  document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+                <i class="fas fa-sign-out"></i>
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
