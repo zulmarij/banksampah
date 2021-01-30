3525b2ec37aaac20f13d259bdaad8cb27dea1a59
Dzul Maarij <fathesshop@gmail.com>
2021-01-30 09:39:57 +0700

    sampahbank

--- app/Http/Controllers/Admin/WarehouseController.php
+++ app/Http/Controllers/Admin/WarehouseController.php
@@ -12,7 +12,7 @@ class WarehouseController extends Controller
     public function index()
     {
         $weight = Warehouse::sum('weight');
-        $trash = Warehouse::where('weight', 'NOT NULL')->count();
+        $trash = Warehouse::where('weight')->notnull()->count();
 
         $total = Warehouse::count();
         $totalPrice = 0;
